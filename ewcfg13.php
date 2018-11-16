d1d4f55c1f611937ccdf0c74eb0b7cb6375dcef7
Fujitsu SH530 <apps.selusin@gmail.com>
2018-11-16 10:44:14 +0700

    1

--- ewcfg13.php
+++ ewcfg13.php
@@ -25,7 +25,7 @@ define("EW_CONFIG_FILE_FOLDER", EW_PROJECT_NAME, TRUE); // Config file name
 define("EW_PROJECT_ID", "{64CABE7A-1609-4157-8293-D7242B591905}", TRUE); // Project ID (GUID)
 $EW_RELATED_PROJECT_ID = "";
 $EW_RELATED_LANGUAGE_FOLDER = "";
-define("EW_RANDOM_KEY", 'VVywC1tJRR4g0bT6', TRUE); // Random key for encryption
+define("EW_RANDOM_KEY", 'qlnxWGat4pf1wlYL', TRUE); // Random key for encryption
 define("EW_PROJECT_STYLESHEET_FILENAME", "phpcss/pembayaran4_prj.css", TRUE); // Project stylesheet file name
 define("EW_CHARSET", "", TRUE); // Project charset
 define("EW_EMAIL_CHARSET", EW_CHARSET, TRUE); // Email charset
